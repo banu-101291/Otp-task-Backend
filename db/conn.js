c08cfadca523c47f064d514cece288973440b54e
banu-101291 <sifnaz78@gmail.com>
2025-01-25 22:11:13 +0530

    route

--- db/conn.js
+++ db/conn.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-const DB = process.env.DATABASE;
+const DB = process.env.MONGO_URL;
 
 mongoose.connect(DB,{
-    useUnifiedTopology:true,
+    //useUnifiedTopology:true,
     useNewUrlParser:true
 }).then(()=>console.log("Database Connected"))
 .catch((error)=>{
     console.log("error",error);
 })
+
+// mongoose.set("strictQuery", false);
+// mongoose.connect(process.env.MONGO_URL, () => {np
+//   console.log("Connected to MongoDB");
+// });
