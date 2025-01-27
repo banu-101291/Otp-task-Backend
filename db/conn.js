ff826a21fb473462097694f3b0a6d1f5db380da8
banu-101291 <sifnaz78@gmail.com>
2025-01-27 20:12:06 +0530

    perser

--- db/conn.js
+++ db/conn.js
@@ -1,17 +1,26 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+// const mongoose = require("mongoose");
+// mongoose.set("strictQuery", false);
 
-const DB = process.env.MONGO_URL;
+// const DB = process.env.MONGO_URL;
 
-mongoose.connect(DB,{
-    //useUnifiedTopology:true,
-    useNewUrlParser:true
-}).then(()=>console.log("Database Connected"))
-.catch((error)=>{
-    console.log("error",error);
-})
+// mongoose.connect(DB,{
+//     useUnifiedTopology:true,
+//     // useNewUrlParser:true
+// }).then(()=>console.log("Database Connected"))
+// .catch((error)=>{
+//     console.log("error",error);
+// })
 
 // mongoose.set("strictQuery", false);
 // mongoose.connect(process.env.MONGO_URL, () => {np
 //   console.log("Connected to MongoDB");
 // });
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false); // Optional: Suppress strictQuery warnings
+
+const DB = process.env.MONGO_URL;
+
+mongoose.connect(DB)
+    .then(() => console.log("Database Connected"))
+    .catch((error) => console.error("Database Connection Error:", error));
